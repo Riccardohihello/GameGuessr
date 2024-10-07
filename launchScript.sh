7bedb16123bc321c850cd3e525f81bd7263d4897
RiccardoHihello <riccardoaiello73@gmail.com>
2024-10-07 09:58:03 +0200

    piccole correzioni ai nomi dei file e aggiunta di qualche commento

---
+++ launchScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# script per avviare tutte le istanze del gioco
+python3 Server.py &
+
+sleep 1
+
+for i in {1..4}
+do
+   python3 Client.py &
+done
+
+read -p "premere invio nel terminale per tgerminare i client"
+
+pkill -f Server.py Client.py
+
